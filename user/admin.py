35491f6a85b2686fa9167d61720a1d7b1563b9ed
datnt1171 <datnt1171@gmail.com>
2025-06-02 16:28:48 +0700

    feat: use UserAdmin

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,50 @@
+# users/admin.py
 from django.contrib import admin
-from .models import User, Role, Department
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Role)
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User, Department, Role
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'department', 'role', 'supervisor')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    
+    # Fields to show in the add user form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 
+                      'department', 'role', 'supervisor', 
+                      'first_name', 'last_name', 'is_active', 'is_staff'),
+        }),
+    )
+    
+    # Fields to show in the change user form
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Work info', {'fields': ('department', 'role', 'supervisor')}),
+    )
+    
+    list_display = ('username', 'email', 'full_name', 'department', 'role', 'is_active')
+    list_filter = ('department', 'role', 'is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Department)
+admin.site.register(Role)
