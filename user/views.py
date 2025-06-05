2227b3392ef56223996b47740aab91c416eebb86
datnt1171 <datnt1171@gmail.com>
2025-06-05 16:23:38 +0700

    feat: hide admin user

--- user/views.py
+++ user/views.py
@@ -1,9 +1,9 @@
-# user/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import UserProfileSerializer, UserListSerializer
+from django.db.models import Q
 
 class UserProfileView(APIView):
     permission_classes = [IsAuthenticated]
@@ -17,6 +17,8 @@ class UserListView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        users = User.objects.active()
+        users = User.objects.active().exclude(
+            Q(role__name__iexact='admin') & Q(department__name__iexact='admin')
+        )
         serializer = UserListSerializer(users, many=True)
         return Response(serializer.data)
