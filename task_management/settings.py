f134d8ef5b55093bb3ef5a7b09eb781319862976
datnt1171 <datnt1171@gmail.com>
2025-06-29 18:52:14 +0700

    fix: sotf disable pagination

--- task_management/settings.py
+++ task_management/settings.py
@@ -172,7 +172,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 1000,
 }
 
 SPECTACULAR_SETTINGS = {
