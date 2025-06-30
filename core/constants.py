bb9e39c09050fa8e6f15aff68fb648bd0c945707
datnt1171 <datnt1171@gmail.com>
2025-06-30 15:47:45 +0700

    feat: add ALLOWED_EXTENSIONS

--- core/constants.py
+++ core/constants.py
@@ -1,2 +1,5 @@
-ALLOWED_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.pdf', '.doc', '.docx', '.xls', '.xlsx']
+ALLOWED_EXTENSIONS = ['.jpg', '.jpeg', '.png', 
+                      '.pdf', '.doc', '.docx', 
+                      '.xls', '.xlsx', 
+                      '.heic', '.heif','.tiff', '.tif']
 MAX_FILE_SIZE = 500 * 1024 * 1024  # 500MB
