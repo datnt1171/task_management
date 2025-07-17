478b35dacd3692a8927620905383a4234e7c72fb
datnt1171 <datnt1171@gmail.com>
2025-07-17 11:05:42 +0700

    fix: fix token live time

--- task_management/settings.py
+++ task_management/settings.py
@@ -178,7 +178,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
