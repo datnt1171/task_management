3cfb226d41c24f8d09bef9aac31bca97e93259e0
datnt1171 <datnt1171@gmail.com>
2025-08-23 09:53:18 +0700

    fix: shorten url

--- sheet/urls.py
+++ sheet/urls.py
@@ -3,8 +3,5 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'finishing-sheets', views.FinishingSheetViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
+router.register(r'sheets', views.FinishingSheetViewSet)
+urlpatterns = router.urls
