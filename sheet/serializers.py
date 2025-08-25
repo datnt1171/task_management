7070c80de275a6178c111875b872e0411362b111
datnt1171 <datnt1171@gmail.com>
2025-08-25 15:54:32 +0700

    feat: always show all translation, disable pagination

--- sheet/serializers.py
+++ sheet/serializers.py
@@ -3,10 +3,30 @@ from django.db import transaction
 from .models import StepTemplate, FormularTemplate, ProductTemplate, FinishingSheet, SheetRow, RowProduct
 
 class StepTemplateSerializer(serializers.ModelSerializer):
+    """Always return all the translation regardless Accept-Language
+        because finising sheet is a snapshot so it wont benefit from FK
+    """
+    name_en = serializers.CharField(read_only=True)
+    name_vi = serializers.CharField(read_only=True)
+    name_zh_hant = serializers.CharField(read_only=True)
     
+    short_name_en = serializers.CharField(read_only=True)
+    short_name_vi = serializers.CharField(read_only=True)
+    short_name_zh_hant = serializers.CharField(read_only=True)
+    
+    spec_en = serializers.CharField(read_only=True)
+    spec_vi = serializers.CharField(read_only=True)
+    spec_zh_hant = serializers.CharField(read_only=True)
+
+
     class Meta:
         model = StepTemplate
-        fields = ('id', 'name', 'short_name', 'spec', 'hold_time', 'consumption')
+        fields = (
+            'id', 'name', 'short_name', 'spec', 'hold_time', 'consumption',
+            'name_en', 'name_vi', 'name_zh_hant',
+            'short_name_en', 'short_name_vi', 'short_name_zh_hant',
+            'spec_en', 'spec_vi', 'spec_zh_hant'
+        )
 
 
 class ProductTemplateSerializer(serializers.ModelSerializer):
@@ -40,7 +60,7 @@ class SheetRowSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = SheetRow
-        fields = ('step_template', 'formular_template', 'step_num', 'spot',
+        fields = ('id', 'step_template', 'formular_template', 'step_num', 'spot',
                   'stepname_en', 'stepname_vi', 'stepname_zh_hant',
                   'viscosity_en', 'viscosity_vi', 'viscosity_zh_hant',
                   'spec_en', 'spec_vi', 'spec_zh_hant',
