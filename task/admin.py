708e1b283abdc722e00dadeae3b9348361dd1028
datnt1171 <datnt1171@gmail.com>
2025-09-24 13:43:22 +0700

    feat: add search by username, filter by state

--- task/admin.py
+++ task/admin.py
@@ -3,6 +3,8 @@ from .models import Task, TaskData, TaskActionLog, TaskPermission
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'state', 'created_at', 'created_by',)
+    list_filter = ('state',)
+    search_fields = ('title', 'created_by__username')
 
 
 class TaskPermissionAdmin(admin.ModelAdmin):
