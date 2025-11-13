ca2dde29e835cb1355182fc2c2552e74688cde27
datnt1171 <datnt1171@gmail.com>
2025-11-13 15:55:44 +0700

    feat: add fleet admin

--- fleet/admin.py
+++ fleet/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Trip, Stop
 
-# Register your models here.
+class StopAdmin(admin.ModelAdmin):
+    list_display = ('location', 'odometer', 'toll_station', 'created_at')
+
+
+class StopInline(admin.TabularInline):
+    model = Stop
+    extra = 0
+    fields = ('order', 'location', 'odometer', 'toll_station')
+    ordering = ('order',)
+    show_change_link = True
+
+
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('date', 'license_plate', 'driver__username', 'created_at')
+    list_filter = ('license_plate',)
+    search_fields = ('driver__username',)
+    ordering = ('date',)
+    inlines = [StopInline]
+
+
+admin.site.register(Trip, TripAdmin)
+admin.site.register(Stop, StopAdmin)
+
